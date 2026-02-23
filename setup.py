e25afaad0b326fba9159983dfad7a01bd1c7a9c5
paulduchesne <paulduchesne@tuta.io>
2026-02-23 20:34:28 +1100

    added mfclient

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="pip_test",
     version="0.1",
-    py_modules=["test_library"], 
+    py_modules=["test_library", 'mfclient'], 
     package_dir={"": "src"},
 )
