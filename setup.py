e55a5771091236dcd6e1ec17b83732338584aac2
Paul Duchesne <paulduchesne@tuta.io>
2026-02-23 20:30:57 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="pip_test",
     version="0.1",
-    packages=find_packages(where="src"),
+    py_modules=["test_library"], 
     package_dir={"": "src"},
 )
